3e5838037909d1e6564fc1e17e83a5b7b03fb50a
Jessica Cooper <jessicagcooper@gmail.com>
2019-11-06 23:09:18 -0600

    Created beginning reducer file, index, & started refactoring App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import { carFeatureReducer } from './reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carFeatureReducer)
+console.log('store', store)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store} ><App /></Provider>, rootElement);
